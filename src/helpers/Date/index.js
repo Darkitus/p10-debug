fbcbd5e6c3b104b5feee7851c7b853f663c0e521
Darkitus <143929120+Darkitus@users.noreply.github.com>
2024-04-07 20:39:49 +0200

    Init project and edited event.json to add id /// helpers/date/index.js to correct the const getMonth

---
+++ src/helpers/Date/index.js
@@ -0,0 +1,16 @@
+export const MONTHS = {
+  1: "janvier",
+  2: "février",
+  3: "mars",
+  4: "avril",
+  5: "mai",
+  6: "juin",
+  7: "juillet",
+  8: "août",
+  9: "septembre",
+  10: "octobre",
+  11: "novembre",
+  12: "décembre",
+};
+
+export const getMonth = (date) => MONTHS[date.getMonth() + 1]; // Ajout de 1 car les mois sont indexés à partir de 0 (particularité de Javascript) donc ça les indexe à partir de 1
